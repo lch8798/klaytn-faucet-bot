215fb39a3ab4529f0f73655372b49e2847353fd8
Chan <33272151+lch8798@users.noreply.github.com>
2020-04-27 21:15:46 +0900

    add. set https

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
+const https = require('https');
 const express = require('express');
+const fs = require('fs');
+
+const config = require('./config.json');
+
 const app = express();
 
-// server config
-const port = 3020;
+const port = config.port;
 
 // client-side react render
 app.use(express.static(__dirname + '/build'));
 
-// app.use('/api', router);
 app.listen(port, () => {
-    console.log(`run node server port: ${port}`);
+    console.log(`run server port: ${port}`);
 });
+
+if (config.https) {
+    const httpsPort = config.httpsPort;
+    const httpsKeys = {
+        key: fs.readFileSync('/../../../../etc/letsencrypt/live/leechan.kr/privkey.pem', 'utf8'),
+        cert: fs.readFileSync('/../../../../etc/letsencrypt/live/leechan.kr/cert.pem', 'utf8'),
+        ca: fs.readFileSync('/../../../../etc/letsencrypt/live/leechan.kr/chain.pem', 'utf8'),
+    };
+
+    const server = https.createServer(httpsKeys, app);
+
+    server.listen(httpsPort, () => {
+        console.log(`run server port: ${httpsPort}`);
+    });
+}
